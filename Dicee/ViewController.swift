7266e785d7fde7fb62e76f2a9590344c8a6fe69b
Your NameVinayak1998git config --global user.name Your <vinayakrrrrr@gmail.com>
2017-12-06 15:16:40 +0530

    New project created

---
+++ Dicee/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Dicee
+//
+//  Created by   Rastogi on 06/12/17.
+//  Copyright © 2017 Vinayak Rastogi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
