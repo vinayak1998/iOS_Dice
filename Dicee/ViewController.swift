0deda9f8d54b6fa929abd6e3d89ff77487c46050
Your NameVinayak1998git config --global user.name Your <vinayakrrrrr@gmail.com>
2017-12-08 16:54:05 +0530

    shake functionality working

--- Dicee/ViewController.swift
+++ Dicee/ViewController.swift
@@ -20,7 +20,8 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        updateDiceImages()
     }
 
     override func didReceiveMemoryWarning() {
@@ -30,13 +31,20 @@ class ViewController: UIViewController {
 
     @IBAction func rollButtonPressed(_ sender: UIButton) {
         
+        updateDiceImages()
+    }
+    
+    func updateDiceImages(){
+        
         randomDiceIndex1 = Int(arc4random_uniform(6))
-        randomDiceIndex1 = Int(arc4random_uniform(6))
+        randomDiceIndex2 = Int(arc4random_uniform(6))
         
         diceImageView1.image = UIImage(named: diceArray[randomDiceIndex1])
         diceImageView2.image = UIImage(named: diceArray[randomDiceIndex2])
-        
     }
     
+    override func motionEnded(_ motion: UIEventSubtype, with event: UIEvent?) {
+        updateDiceImages()
+    }
 }
 
